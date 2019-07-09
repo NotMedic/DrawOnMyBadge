cefa71658d94615082c54c6163bee4b4c7d46c01
Tim McGuffin <tim@mcguffin.org>
2019-07-08 19:12:28 -0500

    Initial Commit

---
+++ DrawOnMyBadge.ino
@@ -0,0 +1,278 @@
+#include "secret.h"
+
+#include <Arduino.h>
+
+#include <ESP8266WiFi.h>
+//#include <WebSocketsServer.h>
+#include <WebSocketsClient.h>
+#include <Hash.h>
+
+// ----------------------------
+// Standard Libraries - Already Installed if you have ESP8266 set up
+// ----------------------------
+
+#include <Ticker.h>
+
+// ----------------------------
+// Additional Libraries - each one of these will need to be installed.
+// ----------------------------
+
+
+#include <PxMatrix.h>
+// The library for controlling the LED Matrix
+// Needs to be manually downloaded and installed
+// https://github.com/2dom/PxMatrix
+
+#define ELEMENTS(x)   (sizeof(x) / sizeof(x[0]))
+
+Ticker display_ticker;
+
+// Pins for LED MATRIX
+#define P_LAT 16
+#define P_A 5
+#define P_B 4
+#define P_C 15
+#define P_OE 2
+#define P_D 12
+#define P_E 0
+
+// PxMATRIX display(32,16,P_LAT, P_OE,P_A,P_B,P_C);
+// PxMATRIX display(64,32,P_LAT, P_OE,P_A,P_B,P_C,P_D);
+PxMATRIX display(64, 32, P_LAT, P_OE, P_A, P_B, P_C, P_D, P_E);
+
+//------- Replace the following! ------
+char ssid[] = WIFI_NAME;       // your network SSID (name)
+char password[] = WIFI_PASS;  // your network key
+
+//WebSocketsServer webSocket = WebSocketsServer(81);
+
+WebSocketsClient webSocket;
+
+void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
+  char xArray[4];
+  int x;
+  int y;
+  int w;
+  int h;
+  uint16_t colour;
+  int commaCount = 0;
+  String inPayload;
+  String colourString;
+
+  int commas[] = { -1, -1, -1, -1}; // using 4 for now
+  int command;
+  switch (type) {
+    case WStype_DISCONNECTED:
+      Serial.println("Disconnected");
+      clearDisplay();
+      display.setCursor(1, 1);
+      display.println("No WS");
+      break;
+    case WStype_CONNECTED:
+      {
+        //        IPAddress ip = webSocket.remoteIP(num);
+        //        Serial.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
+
+        // send message to client
+        webSocket.sendTXT("Badge Connected!");
+        clearDisplay();
+        display.setCursor(1, 1);
+        display.println("WS OK!");
+        delay(500);
+        clearDisplay();
+      }
+      break;
+    case WStype_TEXT:
+      // Serial.printf("[%u] get Text: %s\n", num, payload);
+      inPayload = String((char *) payload);
+      Serial.println(inPayload);
+
+      if (inPayload == "CLEAR") {
+        clearDisplay();
+      } else {
+
+        // clear commas
+        // need to makr this use size of
+        for ( int i = 0; i < ELEMENTS(commas); i++ )
+          commas[i] = -1;
+
+        // grab all comma positions
+        int commaIndex = 0;
+        for (int i = 0; i < inPayload.length(); i++ )
+        {
+          if ( inPayload[i] == ',' )
+            commas[ commaIndex++ ] = i;
+        }
+
+        /* commands
+          0 = draw
+          1 = rect
+          2 = line
+          3 = cirle
+          4 = text ?????
+        */
+
+        // grab command
+        int commandSeperator = inPayload.indexOf(":");
+        command = inPayload.substring(0, commandSeperator).toInt();
+        Serial.println(command);
+
+        x = inPayload.substring(commandSeperator + 1, commas[0]).toInt();
+        y = inPayload.substring(commas[0] + 1, commas[1]).toInt();
+
+//        Serial.print(x);
+//        Serial.print(",");
+//        Serial.println(y);
+
+        if ( command == 0 ) // draw pixel
+        {
+          colourString = inPayload.substring(commas[1] + 1);
+          Serial.println(colourString);
+          colour = strtol(colourString.c_str(), NULL, 0);
+          Serial.println(colour);
+          display.drawPixel(x , y, colour);
+        }
+        else if ( command == 1 ) // rect
+        {
+          w = inPayload.substring(commas[1] + 1, commas[2]).toInt();
+          h = inPayload.substring(commas[2] + 1, commas[3]).toInt();
+          colourString = inPayload.substring(commas[3] + 1);
+          colour = strtol(colourString.c_str(), NULL, 0);
+
+          display.drawRect( x, y, w, h, colour );
+
+        }
+        else if ( command == 2 ) // line
+        {
+          w = inPayload.substring(commas[1] + 1, commas[2]).toInt();
+          h = inPayload.substring(commas[2] + 1, commas[3]).toInt();
+          colourString = inPayload.substring(commas[3] + 1);
+          colour = strtol(colourString.c_str(), NULL, 0);
+
+          display.drawLine( x, y, w, h, colour );
+        }
+        else if ( command == 3 ) // circle
+        {
+          w = inPayload.substring(commas[1] + 1, commas[2]).toInt();
+          colourString = inPayload.substring(commas[2] + 1);
+          colour = strtol(colourString.c_str(), NULL, 0);
+
+          display.drawCircle( x, y, w, colour );
+        }
+        //
+//        else if ( command == 4 ) // text
+//        {
+          //          w = inPayload.substring(commas[1] + 1, commas[2]).toInt();
+          //          colourString = inPayload.substring(commas[2] + 1);
+          //          colour = strtol(colourString.c_str(), NULL, 0);
+
+//          display.setCursor(1, 1);
+          //         display.setTextColor("white");
+//          display.setTextSize(1);
+//          display.setTextWrap(0);
+//          display.println("TESTING");
+
+          //  display.drawCircle( x, y, w, colour );
+//        }
+        //
+
+
+
+
+        // Serial.print("X: ");
+        // Serial.println(x);
+        // Serial.print("Y: ");
+        // Serial.println(y);
+        // Serial.print("Colour: ");
+        // Serial.println(colour, HEX);
+
+
+      }
+
+
+      // send message to client
+      // webSocket.sendTXT(num, "message here");
+
+      // send data to all connected clients
+      // webSocket.broadcastTXT("message here");
+      break;
+    case WStype_BIN:
+      Serial.printf("get binary length: %u\n", length);
+      hexdump(payload, length);
+
+      // send message to client
+      // webSocket.sendBIN(num, payload, length);
+      break;
+  }
+
+}
+
+void display_updater()
+{
+
+  display.display(70);
+
+}
+
+void clearDisplay() {
+  for (int i = 0; i < 64; i++) {
+    for (int j = 0; j < 32; j++) {
+      display.drawPixel(i , j, 0x0000);
+    }
+  }
+}
+
+void setup() {
+
+  Serial.begin(115200);
+
+  display.begin(16);
+  display.clearDisplay();
+
+  display_ticker.attach(0.002, display_updater);
+  yield();
+
+  // Set WiFi to station mode and disconnect from an AP if it was Previously
+  // connected
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+
+  display.setCursor(1, 1);
+  display.setTextSize(1);
+  display.setTextWrap(1);
+
+
+  // Attempt to connect to Wifi network:
+  Serial.print("Connecting Wifi: ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    clearDisplay();
+    display.setCursor(1, 1);
+    display.println("No WiFi");
+
+    delay(500);
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  IPAddress ip = WiFi.localIP();
+  Serial.println(ip);
+
+  clearDisplay();
+  display.setCursor(1, 1);
+  display.println("WiFi: OK");
+
+  //webSocket.begin();
+  webSocket.begin("52.43.252.153", 9001, "/");
+  webSocket.onEvent(webSocketEvent);
+}
+
+void loop() {
+  webSocket.loop();
+
+
+}
