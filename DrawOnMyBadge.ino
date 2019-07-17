9f02ad72dad08fb115b25cea23719d880068ea18
Tim McGuffin <tim@mcguffin.org>
2019-07-17 12:48:28 -0500

    Lots of cleanup, and added JSON functionality

--- DrawOnMyBadge.ino
+++ DrawOnMyBadge.ino
@@ -16,6 +16,8 @@
 #include <WebSocketsClient.h>
 #include <Hash.h>
 
+#include <ArduinoJson.h>
+
 // ----------------------------
 // Standard Libraries - Already Installed if you have ESP8266 set up
 // ----------------------------
@@ -49,6 +51,8 @@ Ticker display_ticker;
 // PxMATRIX display(64,32,P_LAT, P_OE,P_A,P_B,P_C,P_D);
 PxMATRIX display(64, 32, P_LAT, P_OE, P_A, P_B, P_C, P_D, P_E);
 
+//DynamicJsonBuffer jsonBuffer(4096);
+
 //------- Replace the following! ------
 char ssid[] = WIFI_NAME;       // your network SSID (name)
 char password[] = WIFI_PASS;  // your network key
@@ -61,18 +65,13 @@ bool delayRunning = false;
 WebSocketsClient webSocket;
 
 void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
-  char xArray[4];
   int x;
   int y;
-  int w;
-  int h;
   uint16_t colour;
-  int commaCount = 0;
   String inPayload;
   String colourString;
+  //int command;
 
-  int commas[] = { -1, -1, -1, -1}; // using 4 for now
-  int command;
   switch (type) {
     case WStype_DISCONNECTED:
       Serial.println("Disconnected");
@@ -85,129 +84,66 @@ void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
         //        IPAddress ip = webSocket.remoteIP(num);
         //        Serial.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
 
-        // send message to client
-        webSocket.sendTXT("Badge Connected!");
         clearDisplay();
         display.setCursor(1, 1);
         display.println("WS OK!");
         delay(500);
         clearDisplay();
-        webSocket.sendTXT("FRAME");
+
+        webSocket.sendTXT("{\"CMD\":\"FRAME\"}");
       }
       break;
     case WStype_TEXT:
-      // Serial.printf("[%u] get Text: %s\n", num, payload);
-      inPayload = String((char *) payload);
-      Serial.println(inPayload);
+      {
+        // Serial.printf("[%u] get Text: %s\n", num, payload);
+        //      inPayload = String((char *) payload);
 
-      if (inPayload == "CLEAR") {
-        clearDisplay();
-      } else {
+        DynamicJsonBuffer jsonBuffer(4096);
+        JsonObject& root = jsonBuffer.parseObject((char *)payload);
 
-        // clear commas
-        // need to makr this use size of
-        for ( int i = 0; i < ELEMENTS(commas); i++ )
-          commas[i] = -1;
+        String commandtemp = root["CMD"];
 
-        // grab all comma positions
-        int commaIndex = 0;
-        for (int i = 0; i < inPayload.length(); i++ )
-        {
-          if ( inPayload[i] == ',' )
-            commas[ commaIndex++ ] = i;
-        }
+        //      Serial.println("Command: \"" + commandtemp + "\"");
 
-        /* commands
-          0 = draw
-          1 = rect
-          2 = line
-          3 = cirle
-          4 = text ?????
-        */
+        if (commandtemp == "SINGLE") {
 
-        // grab command
-        int commandSeperator = inPayload.indexOf(":");
-        command = inPayload.substring(0, commandSeperator).toInt();
-        Serial.println(command);
+          x = int(root["DATA"][0]);
+          y = int(root["DATA"][1]);
+          String c = root["DATA"][2];
 
-        x = inPayload.substring(commandSeperator + 1, commas[0]).toInt();
-        y = inPayload.substring(commas[0] + 1, commas[1]).toInt();
+          //       Serial.println("x: " + String(x));
+          //       Serial.println("y: " + String(y));
+          //       Serial.println("c: " + c);
 
-//        Serial.print(x);
-//        Serial.print(",");
-//        Serial.println(y);
-
-        if ( command == 0 ) // draw pixel
-        {
-          colourString = inPayload.substring(commas[1] + 1);
-          Serial.println(colourString);
-          colour = strtol(colourString.c_str(), NULL, 0);
-          Serial.println(colour);
+          colour = strtol(c.c_str(), NULL, 0);
+          //        Serial.println(colour);
           display.drawPixel(x , y, colour);
+
+        } else if (commandtemp == "CLEAR") {
+
+          clearDisplay();
+
+        } else if (commandtemp == "DRAW") {
+
+          int node_length = root["DATA"].size();
+
+          for (int i = 0; i < node_length; i++) {
+
+            x = int(root["DATA"][i][0]);
+            y = int(root["DATA"][i][1]);
+            String c = root["DATA"][i][2];
+
+            //        Serial.println("x: " + String(x));
+            //        Serial.println("y: " + String(y));
+            //        Serial.println("c: " + c);
+
+            colour = strtol(c.c_str(), NULL, 0);
+            //        Serial.println(colour);
+            display.drawPixel(x , y, colour);
+
+          }
         }
-        else if ( command == 1 ) // rect
-        {
-          w = inPayload.substring(commas[1] + 1, commas[2]).toInt();
-          h = inPayload.substring(commas[2] + 1, commas[3]).toInt();
-          colourString = inPayload.substring(commas[3] + 1);
-          colour = strtol(colourString.c_str(), NULL, 0);
-
-          display.drawRect( x, y, w, h, colour );
-
-        }
-        else if ( command == 2 ) // line
-        {
-          w = inPayload.substring(commas[1] + 1, commas[2]).toInt();
-          h = inPayload.substring(commas[2] + 1, commas[3]).toInt();
-          colourString = inPayload.substring(commas[3] + 1);
-          colour = strtol(colourString.c_str(), NULL, 0);
-
-          display.drawLine( x, y, w, h, colour );
-        }
-        else if ( command == 3 ) // circle
-        {
-          w = inPayload.substring(commas[1] + 1, commas[2]).toInt();
-          colourString = inPayload.substring(commas[2] + 1);
-          colour = strtol(colourString.c_str(), NULL, 0);
-
-          display.drawCircle( x, y, w, colour );
-        }
-        //
-//        else if ( command == 4 ) // text
-//        {
-          //          w = inPayload.substring(commas[1] + 1, commas[2]).toInt();
-          //          colourString = inPayload.substring(commas[2] + 1);
-          //          colour = strtol(colourString.c_str(), NULL, 0);
-
-//          display.setCursor(1, 1);
-          //         display.setTextColor("white");
-//          display.setTextSize(1);
-//          display.setTextWrap(0);
-//          display.println("TESTING");
-
-          //  display.drawCircle( x, y, w, colour );
-//        }
-        //
-
-
-
-
-        // Serial.print("X: ");
-        // Serial.println(x);
-        // Serial.print("Y: ");
-        // Serial.println(y);
-        // Serial.print("Colour: ");
-        // Serial.println(colour, HEX);
-
-
       }
-
-
-      // send message to client
-      // webSocket.sendTXT(num, "message here");
-
-      // send data to all connected clients
-      // webSocket.broadcastTXT("message here");
       break;
     case WStype_BIN:
       Serial.printf("get binary length: %u\n", length);
@@ -288,10 +224,11 @@ void setup() {
 }
 
 void loop() {
-   if (delayRunning && ((millis() - delayStart) >= 30000)) {
+  if (delayRunning && ((millis() - delayStart) >= 30000)) {
     webSocket.sendPing();
     Serial.println("Sent Ping");
-    delayStart = millis(); 
+    delayStart = millis();
   }
   webSocket.loop();
+  yield();
 }
