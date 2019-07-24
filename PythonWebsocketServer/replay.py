923d1413eb27d1e6c5b9601178044300b26e6006
Tim McGuffin <tim@mcguffin.org>
2019-07-24 12:03:08 -0500

    Update replay.py

--- PythonWebsocketServer/replay.py
+++ PythonWebsocketServer/replay.py
@@ -1,15 +1,30 @@
 #!/usr/bin/python
+#cat file | ./replay.py
 
 from ws4py.client import WebSocketBaseClient
 import time
 import sys
+import json
 
-ws = WebSocketBaseClient('ws://52.43.252.153:9001/')
+ws = WebSocketBaseClient('ws://127.0.0.1:9001/')
 ws.connect()
 
-for line in sys.stdin:
-    print(line)
-    ws.send(line)
-    time.sleep(.02)
 
-ws.close()
+k=0
+data ={ 'CMD': [], 'DATA': []}
+
+for line in sys.stdin:
+    line2 = line.replace(':',',').rstrip()
+    rawdata = line2.split(',')
+    pixels=[]
+    pixels.append(rawdata[1])
+    pixels.append(rawdata[2])
+    pixels.append(rawdata[3])
+
+    data["DATA"].append(pixels)
+    data.update({"CMD" : "DRAW"})
+    json_data = json.dumps(data)
+    ws.send(json_data)
+    time.sleep(0.007)
+    k = 0
+    data["DATA"] *= 0
